4f0793bc92c936915bb9a238b0b3c1e613621b22
Jeff Eldredge <jdeldre@g.ucla.edu>
2020-12-18 13:05:17 -0800

    Added types and algorithms for extension from OrdinaryDiffEq

---
+++ src/timemarching/algorithms.jl
@@ -0,0 +1,199 @@
+## Definitions of algorithms ##
+
+# The sc parameter specifies whether it contains static constraint operators or not
+# If false, then it expects that the state vector contains a component for updating the opertors
+
+# WrayHERK is scheme C in Liska and Colonius (JCP 2016)
+# BraseyHairerHERK is scheme B in Liska and Colonius (JCP 2016)
+# LiskaIFHERK is scheme A in Liska and Colonius (JCP 2016)
+
+abstract type ConstrainedOrdinaryDiffEqAlgorithm{sc} <: OrdinaryDiffEq.OrdinaryDiffEqAlgorithm end
+
+for (Alg,Order) in [(:WrayHERK,3),(:BraseyHairerHERK,3),(:LiskaIFHERK,2),(:IFHEEuler,1)]
+    @eval struct $Alg{sc,solverType} <: ConstrainedOrdinaryDiffEqAlgorithm{sc}
+    end
+
+    @eval $Alg(;static_constraints=true,saddlesolver=ViscousFlow.ConstrainedSystems.Direct) = $Alg{static_constraints,saddlesolver}()
+
+    @eval export $Alg
+
+    @eval alg_order(alg::$Alg) = $Order
+end
+
+### Caches ###
+
+# LiskaIFHERK
+
+@cache struct LiskaIFHERKCache{sc,solverType,uType,rateType,expType1,expType2,saddleType,pType,TabType} <: OrdinaryDiffEqMutableCache
+  u::uType
+  uprev::uType # qi
+  k1::rateType # w1
+  k2::rateType # w2
+  k3::rateType # w3
+  utmp::uType  # cache
+  dutmp::rateType # cache for rates
+  fsalfirst::rateType
+  Hhalfdt::expType1
+  Hzero::expType2
+  S::saddleType
+  pold::pType
+  k::rateType
+  tab::TabType
+end
+
+struct LiskaIFHERKConstantCache{T,T2} <: OrdinaryDiffEqConstantCache
+  ã11::T
+  ã21::T
+  ã22::T
+  ã31::T
+  ã32::T
+  ã33::T
+  c̃1::T2
+  c̃2::T2
+  c̃3::T2
+
+  function LiskaIFHERKConstantCache(T, T2)
+    ã11 = T(1//2)
+    ã21 = T(√3/3)
+    ã22 = T((3-√3)/3)
+    ã31 = T((3+√3)/6)
+    ã32 = T(-√3/3)
+    ã33 = T((3+√3)/6)
+    c̃1 = T2(1//2)
+    c̃2 = T2(1.0)
+    c̃3 = T2(1.0)
+    new{T,T2}(ã11,ã21,ã22,ã31,ã32,ã33,c̃1,c̃2,c̃3)
+  end
+end
+
+LiskaIFHERKCache{sc,solverType}(u,uprev,k1,k2,k3,utmp,dutmp,fsalfirst,
+                                Hhalfdt,Hzero,S,p,k,tab) where {sc,solverType} =
+        LiskaIFHERKCache{sc,solverType,typeof(u),typeof(k1),typeof(Hhalfdt),typeof(Hzero),
+                        typeof(S),typeof(p),typeof(tab)}(u,uprev,k1,k2,k3,utmp,dutmp,fsalfirst,
+                                                          Hhalfdt,Hzero,S,p,k,tab)
+
+
+function alg_cache(alg::LiskaIFHERK{sc,solverType},u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,uprev2,f,t,dt,reltol,p,calck,::Val{true}) where {sc,solverType}
+
+  typeof(u) <: ArrayPartition || error("u must be of type ArrayPartition")
+
+  y, z = u.x[1], u.x[2]
+
+  utmp = zero(u)
+  k1, k2, k3, dutmp, fsalfirst, k = (zero(rate_prototype) for i in 1:6)
+
+  tab = LiskaIFHERKConstantCache(constvalue(uBottomEltypeNoUnits), constvalue(tTypeNoUnits))
+
+  A = f.odef.f1.f
+  Hhalfdt = exp(A,-dt/2,y)
+  Hzero = exp(A,zero(dt),y)
+
+  S1 = saddle_system(Hhalfdt,f,p,p,dutmp,solverType)
+  S = [S1]
+  push!(S,saddle_system(Hzero,f,p,p,dutmp,solverType))
+
+  LiskaIFHERKCache{sc,solverType}(u,uprev,k1,k2,k3,utmp,dutmp,fsalfirst,Hhalfdt,Hzero,S,[p],k,tab)
+end
+
+function alg_cache(alg::LiskaIFHERK,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,uprev2,f,t,dt,reltol,p,calck,::Val{false})
+  LiskaIFHERKConstantCache(constvalue(uBottomEltypeNoUnits), constvalue(tTypeNoUnits))
+end
+
+function saddle_system(A,f,p,pold,ducache,solver)
+    nully, nullz = state(ducache), constraint(ducache)
+    B₁ᵀ(z) = (ducache .= 0.0; nully .= 0; -_ode_neg_B1!(ducache,f,ArrayPartition(nully,z),pold,0.0))
+    B₂(y) = (ducache .= 0.0; nullz .= 0; -_constraint_neg_B2!(ducache,f,ArrayPartition(y,nullz),p,0.0))
+    SaddleSystem(A,B₂,B₁ᵀ,ducache,solver=solver)
+end
+
+@inline saddle_system!(S::SaddleSystem,A,f,p,pold,ducache,solver,::Val{false}) = (S = saddle_system(A,f,p,pold,ducache,solver); nothing)
+@inline saddle_system!(S::SaddleSystem,A,f,p,pold,ducache,solver,::Val{true}) = nothing
+
+function initialize!(integrator,cache::LiskaIFHERKCache)
+    @unpack k,fsalfirst = cache
+
+    integrator.fsalfirst = fsalfirst
+    integrator.fsallast = k
+    integrator.kshortsize = 1
+    resize!(integrator.k, integrator.kshortsize)
+    integrator.k[1] = integrator.fsalfirst
+    integrator.f.odef(integrator.fsalfirst, integrator.uprev, integrator.p, integrator.t) # Pre-start fsal
+    integrator.destats.nf += 1
+
+end
+
+@muladd function perform_step!(integrator,cache::LiskaIFHERKCache{sc,solverType},repeat_step=false) where {sc,solverType}
+    @unpack t,dt,uprev,u,f,p = integrator
+    @unpack k1,k2,k3,utmp,dutmp,fsalfirst,Hhalfdt,Hzero,S,pold,k = cache
+    @unpack ã11,ã21,ã22,ã31,ã32,ã33,c̃1,c̃2,c̃3 = cache.tab
+    @unpack param_update_func = f
+
+    # aliases to the state and constraint parts
+    ytmp, ztmp = state(utmp), constraint(utmp)
+    yprev = state(uprev)
+    z = constraint(u)
+
+    ttmp = t
+    u .= uprev
+
+    _ode_r1!(k1,f,u,p,ttmp)  # should be able to replace this with fsalfirst
+    integrator.destats.nf += 1
+    @.. k1 *= dt*ã11
+    @.. utmp = uprev + k1
+
+    # if applicable, update p, construct new saddle system here, using Hhalfdt
+    pold[1] = p
+    param_update_func(p,utmp,pold[1],ttmp)
+    saddle_system!(S[1],Hhalfdt,f,p,pold[1],dutmp,solverType,Val(sc))
+
+    _constraint_r2!(utmp,f,u,p,ttmp) # this should only update the z part
+    u .= S[1]\utmp
+
+    ytmp .= typeof(ytmp)(S[1].A⁻¹B₁ᵀf)
+    ttmp = t + dt*c̃1
+
+    ldiv!(yprev,Hhalfdt,yprev)
+    ldiv!(k1.x[1],Hhalfdt,k1.x[1])
+
+    @.. k1 = (k1-utmp)/(dt*ã11)
+
+    _ode_r1!(k2,f,u,p,ttmp)
+    integrator.destats.nf += 1
+    @.. k2 *= dt*ã22
+    @.. utmp = uprev + k2 + dt*ã21*k1
+
+    # if applicable, update p, construct new saddle system here, using Hhalfdt
+    pold[1] = p
+    param_update_func(p,utmp,pold[1],ttmp)
+    saddle_system!(S[1],Hhalfdt,f,p,pold[1],dutmp,solverType,Val(sc))
+
+    _constraint_r2!(utmp,f,u,p,ttmp)
+    u .= S[1]\utmp
+    ytmp .= typeof(ytmp)(S[1].A⁻¹B₁ᵀf)
+    ttmp = t + dt*c̃2
+
+    ldiv!(yprev,Hhalfdt,yprev)
+    ldiv!(k1.x[1],Hhalfdt,k1.x[1])
+    ldiv!(k2.x[1],Hhalfdt,k2.x[1])
+
+    @.. k2 = (k2-utmp)/(dt*ã22)
+    _ode_r1!(k3,f,u,p,ttmp)
+    integrator.destats.nf += 1
+    @.. k3 *= dt*ã33
+    @.. utmp = uprev + k3 + dt*ã32*k2 + dt*ã31*k1
+
+    # if applicable, update p, construct new saddle system here, using Hzero (identity)
+    pold[1] = p
+    param_update_func(p,utmp,pold[1],ttmp)
+    saddle_system!(S[2],Hzero,f,p,pold[1],dutmp,solverType,Val(sc))
+
+    _constraint_r2!(utmp,f,u,p,ttmp)
+    u .= S[2]\utmp
+
+    @.. z /= dt*ã33
+
+    f.odef(k, u, p, t+dt)
+
+    integrator.destats.nf += 1
+    return nothing
+end
---
+++ src/timemarching/types.jl
@@ -0,0 +1,129 @@
+import OrdinaryDiffEq: OrdinaryDiffEqAlgorithm, alg_order, alg_cache,
+                    OrdinaryDiffEqMutableCache, OrdinaryDiffEqConstantCache, @cache,
+                    initialize!, perform_step!, @muladd, @unpack, constvalue,
+                    full_cache, @..
+
+import OrdinaryDiffEq.DiffEqBase: AbstractDiffEqLinearOperator,
+                                  DEFAULT_UPDATE_FUNC, has_exp,
+                                  AbstractODEFunction, isinplace
+
+export DiffEqLinearOperator, ConstrainedODEFunction
+
+#### Operator and function types ####
+
+"""
+$(TYPEDEF)
+"""
+mutable struct DiffEqLinearOperator{T,aType} <: AbstractDiffEqLinearOperator{T}
+    A :: aType
+    DiffEqLinearOperator(A::aType; update_func=DEFAULT_UPDATE_FUNC,
+                          dtype=Float64) where {aType} = new{dtype,aType}(A)
+end
+
+(f::DiffEqLinearOperator)(du,u,p,t) = mul!(du.x[1],f.A,u.x[1])
+
+import Base: exp
+exp(f::DiffEqLinearOperator,args...) = exp(f.A,args...)
+has_exp(::DiffEqLinearOperator) = true
+
+
+"""
+        ConstrainedODEFunction(r1,r2,B1,B2[,A=I])
+
+This specifies the functions and operators that comprise an ODE problem with the form
+
+``
+\\dfrac{dy}{dt} = Ay - B_1 z + r_1(y,t)
+``
+
+``
+B_2 y = r_2(t)
+``
+
+The optional linear operator `A` defaults to the identity. The `B1` and `B2` functions must be of the respective
+in-place forms `B1(dy,z,p)` (to compute the action of `B1` on `z`) and `B2(dz,y,p)` (to compute that action
+of `B2` on `y`). The function `r1` must of the in-place form `r1(dy,y,p,t)`, and `r2` must be in the in-place form
+`r2(dz,p,t)`.
+
+An optional argument `param_update_func` can be used to set a function that updates problem parameters with
+the current solution. This function must take the in-place form `f(q,u,p,t)` to update some `q`. (`q` might
+simply be `p`.)
+"""
+struct ConstrainedODEFunction{iip,static,F1,F2,TMM,C,Ta,Tt,TJ,JVP,VJP,JP,SP,TW,TWt,TPJ,S,TCV,PF} <: AbstractODEFunction{iip}
+    odef :: F1
+    conf :: F2
+    mass_matrix::TMM
+    cache::C
+    analytic::Ta
+    tgrad::Tt
+    jac::TJ
+    jvp::JVP
+    vjp::VJP
+    jac_prototype::JP
+    sparsity::SP
+    Wfact::TW
+    Wfact_t::TWt
+    paramjac::TPJ
+    syms::S
+    colorvec::TCV
+    param_update_func :: PF
+end
+
+function ConstrainedODEFunction(r1,r2,B1,B2,A=I; param_update_fcn = DEFAULT_UPDATE_FUNC,
+                                                 mass_matrix=I,_func_cache=nothing,
+                                                 analytic=nothing,
+                                                 tgrad = nothing,
+                                                 jac = nothing,
+                                                 jvp=nothing,
+                                                 vjp=nothing,
+                                                 jac_prototype = nothing,
+                                                 sparsity=jac_prototype,
+                                                 Wfact = nothing,
+                                                 Wfact_t = nothing,
+                                                 paramjac = nothing,
+                                                 syms = nothing,
+                                                 colorvec = nothing)
+
+    if isinplace(r1,4) && isinplace(r2,3) && isinplace(B1,3) && isinplace(B2,3)
+        @inline r1ext(du,u,p,t) = r1(state(du),state(u),p,t)
+        @inline r2ext(du,u,p,t) = r2(constraint(du),p,t)
+        @inline B1ext_rhs(du,u,p,t) = (dy = state(du); B1(dy,constraint(u),p); dy .*= -1)
+        @inline B2ext_rhs(du,u,p,t) = (dz = constraint(du); B2(dz,state(u),p); dz .*= -1)
+    elseif isinplace(r1,3) && isinplace(r2,2) && isinplace(B1,2) && isinplace(B2,2)
+        @inline r1ext(u,p,t) = r1(state(u),p,t)
+        @inline r2ext(u,p,t) = r2(p,t)
+        @inline B1ext_rhs(u,p,t) = -B1(constraint(u),p)
+        @inline B2ext_rhs(u,p,t) = -B2(state(u),p)
+    else
+        error("Inconsistent function signatures")
+    end
+
+    odef_nl = SplitFunction(r1ext,B1ext_rhs;_func_cache=deepcopy(_func_cache))
+    odef = SplitFunction(A, odef_nl ;_func_cache=deepcopy(_func_cache))
+    conf = SplitFunction(r2ext,B2ext_rhs;_func_cache=deepcopy(_func_cache))
+
+    static = param_update_fcn == DEFAULT_UPDATE_FUNC ? true : false
+
+    ConstrainedODEFunction{isinplace(odef),static,typeof(odef),
+                           typeof(conf),typeof(mass_matrix),typeof(_func_cache),
+                           typeof(analytic),typeof(tgrad),typeof(jac),typeof(jvp),typeof(vjp),
+                           typeof(jac_prototype),typeof(sparsity),
+                           typeof(Wfact),typeof(Wfact_t),typeof(paramjac),typeof(syms),
+                           typeof(colorvec),typeof(param_update_fcn)}(odef,conf,mass_matrix,_func_cache,
+                           analytic,tgrad,jac,jvp,vjp,jac_prototype,
+                           sparsity,Wfact,Wfact_t,paramjac,syms,colorvec,param_update_fcn)
+end
+
+@inline _ode_A!(du,f::ConstrainedODEFunction,u,p,t) = f.odef.f1(du,u,p,t) # A
+@inline _ode_r1!(du,f::ConstrainedODEFunction,u,p,t) = f.odef.f2.f.f1(du,u,p,t) # r_1
+
+@inline _ode_neg_B1!(du,f::ConstrainedODEFunction,u,p,t) = f.odef.f2.f.f2(du,u,p,t) # -B_1^T
+@inline _constraint_neg_B2!(du,f::ConstrainedODEFunction,u,p,t) = f.conf.f2(du,u,p,t) # -B_2
+@inline _constraint_r2!(du,f::ConstrainedODEFunction,u,p,t) = f.conf.f1(du,u,p,t) # r_2
+
+
+function (f::ConstrainedODEFunction)(du,u,p,t)
+    f.odef(f.cache,u,p,t)
+    f.conf(du,u,p,t)
+    du .+= f.cache
+end
